8a024b366b417b670c7243f603425df193bbc6c6
Kravalg <kravtsov.yar@gmail.com>
2025-05-18 21:17:51 +0200

    feat(#2): implement api gateway poc

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,67 +1,61 @@
 import pulumi
 import pulumi_aws as aws
 
-from config.config import Config
-from components.certificate import Certificate
-from components.vpc import VPC
-from components.api_gateway import APIGateway
-from components.dns import DNS
+# -----------------------------------------------------------
+# 1. Configuration
+# -----------------------------------------------------------
+config = pulumi.Config("vilnacrm")
+region = config.require("region")
+app_runner_url = config.require("app_runner_url")
 
-config = Config()
-
-subdomain = config.full_subdomain
-domain_name = config.domain_name
-region = config.region
-vpc_cidr = config.vpc_cidr
-endpoint_service_name = config.endpoint_service_name
-
-# Fetch the existing Route53 hosted zone
-hosted_zone = aws.route53.get_zone(name=domain_name)
-
-# Request an ACM certificate for the subdomain
-certificate = Certificate(subdomain, hosted_zone.zone_id)
-
-# Create a VPC for API Gateway's VPC Link
-vpc = VPC(name="api", cidr_block=vpc_cidr, region=region)
-
-# Create an Interface VPC Endpoint that connects to the endpoint service in Account B
-vpc_endpoint = aws.ec2.VpcEndpoint("interfaceVpcEndpoint",
-    vpc_id=vpc.vpc.id,
-    service_name=endpoint_service_name,
-    vpc_endpoint_type="Interface",
-    security_group_ids=[vpc.security_group.id],
-    subnet_ids=[vpc.subnet1.id, vpc.subnet2.id],
-    private_dns_enabled=False,  # Set to False if the endpoint service does not support private DNS
-    tags={
-        "Name": "interface-vpc-endpoint",
-    })
-
-# Create an API Gateway VPC Link
-vpc_link = aws.apigatewayv2.VpcLink("apiVpcLink",
-    name="api-vpc-link",
-    subnet_ids=[vpc.subnet1.id, vpc.subnet2.id],
-    security_group_ids=[vpc.security_group.id],
-    tags={
-        "Name": "api-vpc-link",
-    })
-
-# Set up API Gateway
-api_gateway = APIGateway(
-    name="api",
-    vpc_link=vpc_link,
-    vpc_endpoint=vpc_endpoint,
-    subdomain=subdomain,
-    certificate_arn=certificate.certificate_arn
+# -----------------------------------------------------------
+# 2. Create an HTTP API Gateway
+# -----------------------------------------------------------
+api = aws.apigatewayv2.Api(
+    "httpApi",
+    protocol_type="HTTP"
 )
 
-# Set up DNS records
-dns = DNS(
-    subdomain=subdomain,
-    hosted_zone_id=hosted_zone.zone_id,
-    api_domain_name=api_gateway.api_domain_name
+# -----------------------------------------------------------
+# 3. Set Up Greedy Proxy Integration for /user/*
+# -----------------------------------------------------------
+# The {proxy} placeholder will be replaced with whatever path segment(s)
+# matched by the route's {proxy+} variable.
+integration = aws.apigatewayv2.Integration(
+    "userProxyIntegration",
+    api_id=api.id,
+    integration_type="HTTP_PROXY",
+    integration_method="ANY",
+    # Embed {proxy} directly in the URI so API Gateway forwards the full path.
+    integration_uri=f"{app_runner_url}/user/{{proxy}}"
 )
 
-# Export the API endpoint URL
-pulumi.export("apiEndpoint", api_gateway.api_domain_name.domain_name)
+# -----------------------------------------------------------
+# 4. Catch-All Route for /user/{proxy+}
+# -----------------------------------------------------------
+# ANY method, greedy path match under /user.
+route = aws.apigatewayv2.Route(
+    "userProxyRoute",
+    api_id=api.id,
+    route_key="ANY /user/{proxy+}",
+    target=pulumi.Output.concat("integrations/", integration.id)
+)
 
-# TODO: remove all manual steps that we need to perform in cli for make plan and make up (automate aws configure, secret password)
+# -----------------------------------------------------------
+# 5. Deploy on the Default Stage
+# -----------------------------------------------------------
+# AWS requires a non-empty name; using the reserved `$default` yields
+# an invoke URL without a stage-name suffix.
+stage = aws.apigatewayv2.Stage(
+    "apiStage",
+    api_id=api.id,
+    name="$default",
+    auto_deploy=True,
+    opts=pulumi.ResourceOptions(depends_on=[route])
+)
+
+# -----------------------------------------------------------
+# 6. Export the API Endpoint
+# -----------------------------------------------------------
+# Clients will call this without any extra /<stage> segment.
+pulumi.export("apiEndpoint", stage.invoke_url)
